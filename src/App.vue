e9c6f13a363743b28d9940b2483136110ce1d736
BetterCallAle <dap22091998@gmail.com>
2022-11-08 15:09:45 +0100

    initialized sass

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,17 @@ export default{
 </script>
 
 <template>
+<ul>
+  <li>Ciao</li>
+  <li>Ciao</li>
+  <li>Ciao</li>
+</ul>
 
 </template>
 
 <style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables' as *;
+@use './styles/partials/mixins' as *;
+
 </style>
